c6c5c7325c01ad1a88c5f7048a22d6fde9e2dbc4
Saumya101 <s.ashokkumar.patel@accenture.com>
2025-04-11 12:17:55 +0530

    dashboard commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { CssBaseline, Box, useMediaQuery } from '@mui/material';
+import NavigationBar from './NavigationBar';
+import SideDrawer from './SideDrawer';
+import MainContent from './MainContent';
+import { ThemeProviderComponent } from './ThemeContext';
 
-function App() {
+const App = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (state) => () => {
+    setDrawerOpen(state);
+  };
+
+  const isSmallScreen = useMediaQuery('(max-width:600px)');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProviderComponent>
+      <CssBaseline />
+      <NavigationBar toggleDrawer={toggleDrawer} />
+
+      <Box display="flex">
+        <SideDrawer
+          open={!isSmallScreen || drawerOpen}
+          onToggleDrawer={toggleDrawer}
+          variant={isSmallScreen ? 'temporary' : 'persistent'}
+        />
+
+        <Box
+          component="main"
+          style={{
+            flexGrow: 1,
+            marginTop: '64px',
+            marginLeft: isSmallScreen ? 0 : '240px',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <MainContent />
+        </Box>
+      </Box>
+    </ThemeProviderComponent>
   );
-}
+};
 
 export default App;
---
+++ src/MainContent.js
@@ -0,0 +1,493 @@
+import React, { useState, useEffect } from 'react';
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Select, MenuItem, FormControl, Typography, FormLabel, TextField } from '@mui/material';
+import { Pie, Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+} from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement);
+
+const MainContent = () => {
+  const [, setData] = useState([]);
+  const [salesData, setSalesData] = useState([]);
+  const [totalSales, setTotalSales] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+  const [discount, setDiscount] = useState(0);
+  const [profit, setProfit] = useState(0);
+  const [aggregatedData, setAggregatedData] = useState([]);
+  const [stateName, setStateName] = useState([]);
+  const [selectedState, setSelectedState] = useState('');
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [filteredData, setFilteredData] = useState([]);
+
+  const handleStartDateChange = (date) => {
+    if (endDate && date > endDate) {
+      alert("Start Date cannot be later than End Date!");
+    } else {
+      setStartDate(date);
+    }
+  };
+
+  const handleEndDateChange = (date) => {
+    if (startDate && date < startDate) {
+      alert("End Date cannot be earlier than Start Date!");
+    } else {
+      setEndDate(date);
+    }
+  };
+
+  useEffect(() => {
+    fetch('/sales.json')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((salesData) => setSalesData(salesData))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  useEffect(() => { 
+    var state = [];
+    if (salesData.length > 0) {
+      // eslint-disable-next-line array-callback-return
+      salesData.map((item) => {
+        state.push(item.State);
+      })
+      let uniqueArray = [...new Set(state)];
+      setStateName(uniqueArray.sort());
+      setSelectedState(uniqueArray.sort()[0]);
+     }
+  }, [salesData]);
+
+  useEffect(() => { 
+    const filteredData = salesData.filter((item) => {
+      const currentDate = new Date(item["Order Date"]);
+      const StartDate = new Date(startDate);
+      const EndDate = new Date(endDate);
+
+      const isWithinDateRange = (!startDate || currentDate >= StartDate) &&
+                            (!endDate || currentDate <= EndDate);
+
+      const isMatchingState = !selectedState || item.State === selectedState;
+
+      return isWithinDateRange && isMatchingState;
+  });
+    setFilteredData(filteredData);
+  }, [salesData, selectedState, startDate, endDate]);
+
+
+  useEffect(() => {
+    var totalSales = 0;
+    var quantity = 0;
+    var discount = 0;
+    var profit = 0;
+    var cityAggregation = {};
+    if (filteredData.length > 0) {
+      // eslint-disable-next-line array-callback-return
+      filteredData.map((item) => {
+          totalSales += item.Sales;
+          quantity += item.Quantity;
+          discount += item.Discount;
+          profit += item.Profit;
+          cityAggregation = filteredData.reduce((acc, item) => {
+              if (!acc[item.City]) {
+                acc[item.City] = 0;
+              }
+              acc[item.City] += item.Quantity;
+              return acc;
+        }, {});
+      })
+      setTotalSales(totalSales);
+      setQuantity(quantity);
+      setDiscount(discount);
+      setProfit(profit);
+      const cities = Object.keys(cityAggregation);
+      const quantities = Object.values(cityAggregation);
+      setAggregatedData({ cities, quantities });
+     }
+      
+  }, [endDate, filteredData, salesData, selectedState, startDate]);
+
+  useEffect(() => {
+    fetch('/sampleData.json')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => setData(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  const unqCategory = [...new Set(filteredData.map((item) => item["Category"]))];
+    const groupedSales = filteredData.reduce((accumulator, current) => {
+    if (!accumulator[current.Category]) {
+      accumulator[current.Category] = 0;
+    }
+    accumulator[current.Category] += current.Sales;
+      return accumulator;
+    }, {});
+
+    const categoryResult = Object.keys(groupedSales).map((Category) => ({
+    category: Category,
+    totalSales: groupedSales[Category]
+  }));
+
+  const unqSegment = [...new Set(filteredData.map((item) => item["Segment"]))];
+    const groupedSengmentSales = filteredData.reduce((accumulator, current) => {
+    if (!accumulator[current.Segment]) {
+      accumulator[current.Segment] = 0;
+    }
+    accumulator[current.Segment] += current.Sales; 
+      return accumulator;
+    }, {});
+    const segmentResult = Object.keys(groupedSengmentSales).map((Segment) => ({
+    segment: Segment,
+    totalSales: groupedSengmentSales[Segment]
+  }));
+
+  // eslint-disable-next-line no-unused-vars
+  const unqSubCategory = [...new Set(filteredData.map((item) => item["Sub-Category"]))];
+    const groupedSubSales = filteredData.reduce((accumulator, current) => {
+    if (!accumulator[current["Sub-Category"]]) {
+      accumulator[current["Sub-Category"]] = 0;
+    }
+    accumulator[current["Sub-Category"]] += current.Sales;
+      return accumulator;
+    }, {});
+    const SubResult = Object.keys(groupedSubSales).map((Sub) => ({
+    sub: Sub,
+      totalSales: groupedSubSales[Sub]
+  }));
+  
+  // eslint-disable-next-line no-unused-vars
+  const unqProductName = [...new Set(filteredData.map((item) => item["Product Name"]))];
+    const groupedProductName = filteredData.reduce((accumulator, current) => {
+    if (!accumulator[current["Product Name"]]) {
+      accumulator[current["Product Name"]] = 0;
+    }
+    accumulator[current["Product Name"]] += current.Sales;
+      return accumulator;
+    }, {});
+    const ProductNameResult = Object.keys(groupedProductName).map((Product) => ({
+    product: Product,
+    totalSales: groupedProductName[Product]
+  }));
+
+  const pieChartData = {
+    labels: unqCategory,
+    datasets: [
+      {
+        data: categoryResult.map((item) => item.totalSales),
+        backgroundColor: [
+          '#D26E64',
+          '#FFBF65',
+          '#227CB4',
+        ],
+      },
+    ],
+  };
+
+  const pieChartDataSegment = {
+    labels: unqSegment,
+    datasets: [
+      {
+        data: segmentResult.map((item) => item.totalSales),
+        backgroundColor: [
+          '#D26E64',
+          '#FFBF65',
+          '#227CB4',
+        ],
+      },
+    ],
+  };
+
+  const barChartData = {
+    labels: aggregatedData.cities,
+    datasets: [
+      {
+        data: aggregatedData.quantities,
+        backgroundColor: '#8BD0E0',
+        color: '#fff',
+      },
+    ],
+  };
+
+  const barChartOptions = {
+    responsive: true,
+    indexAxis: 'y',
+    plugins: { legend: { position: 'top' } },
+  };
+
+  return (
+    <div style={{ padding: '20px', backgroundColor: '#161a33' }}>
+      <Box
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginBottom: '20px',
+        }}
+      >
+        <Typography variant="h5" style={{ fontWeight: 'bold', color: '#fff' }}>
+          Sales Overview
+        </Typography>
+
+        <Box style={{ display: 'flex', gap: '15px' }}>
+          <FormControl size="small" style={{ minWidth: 120 }}>
+      <FormLabel component="legend" style={{ marginBottom: '10px', color: '#fff' }}>Select State</FormLabel>
+            <Select variant="outlined" fullWidth value={selectedState} onChange={(e) => setSelectedState(e.target.value)} style={{ color: '#fff' }}>
+              {stateName.map((item) => (
+                <MenuItem key={item} value={item} style={{ color: '#fff' }}>
+                  {item}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl size="small" style={{ minWidth: 120 }}>
+      <FormLabel component="legend" style={{ marginBottom: '10px', color: '#fff' }}>Select From date</FormLabel>
+      <TextField
+        type="date"
+        value={startDate}
+        onChange={e => handleStartDateChange(e.target.value)}
+        variant="outlined"
+        fullWidth
+        InputProps={{
+          style: { color: '#fff' },
+        }}
+      />
+    </FormControl>
+
+          <FormControl size="small" style={{ minWidth: 120 }}>
+      <FormLabel component="legend" style={{ marginBottom: '10px', color: '#fff' }}>Select To date</FormLabel>
+      <TextField
+        type="date"
+        value={endDate}
+        onChange={e => handleEndDateChange(e.target.value)}
+        variant="outlined"
+        fullWidth
+        InputProps={{
+          style: { color: '#fff' },
+        }}
+      />
+    </FormControl>
+        </Box>
+      </Box>
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+          <div
+            style={{
+              width: '22%',
+              height: '150px',
+              backgroundColor: '#262D47',
+              borderRadius: '10px',
+              boxShadow: '0px 4px 8px rgba(0,0,0,0.1)',
+              padding: '10px',
+            }}
+        >
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100%' }}>
+            <div>
+            <img src="https://img.icons8.com/ios-filled/50/ffffff/sales-performance.png" alt="sales" style={{ width: '50px', height: '50px', marginLeft: '40%' }} />
+            </div>
+            <div>
+              <h3 style={{ textAlign: 'center', color: '#fff' }}>Total Sales</h3>
+              <p style={{ textAlign: 'center', color: '#fff' }}>{totalSales}</p>
+            </div>
+            </div>
+          </div>
+          <div
+            style={{
+              width: '22%',
+              height: '150px',
+              backgroundColor: '#262D47',
+              borderRadius: '10px',
+              boxShadow: '0px 4px 8px rgba(0,0,0,0.1)',
+              padding: '10px',
+            }}
+        >
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100%' }}>
+            <div>
+            <img src="https://img.icons8.com/ios-filled/50/ffffff/sales-performance.png" alt="sales" style={{ width: '50px', height: '50px', marginLeft: '40%' }} />
+            </div>
+            <div>
+              <h3 style={{ textAlign: 'center', color: '#fff' }}>Quantity Sold</h3>
+              <p style={{ textAlign: 'center', color: '#fff' }}>{quantity}</p>
+            </div>
+            </div>
+        </div>
+        <div
+            style={{
+              width: '22%',
+              height: '150px',
+              backgroundColor: '#262D47',
+              borderRadius: '10px',
+              boxShadow: '0px 4px 8px rgba(0,0,0,0.1)',
+              padding: '10px',
+            }}
+          >
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100%' }}>
+            <div>
+            <img src="https://img.icons8.com/ios-filled/50/ffffff/sales-performance.png" alt="sales" style={{ width: '50px', height: '50px', marginLeft: '40%' }} />
+            </div>
+            <div>
+              <h3 style={{ textAlign: 'center', color: '#fff' }}>Discount%</h3>
+              <p style={{ textAlign: 'center', color: '#fff' }}>{discount}</p>
+            </div>
+          </div>
+        </div>
+        <div
+            style={{
+              width: '22%',
+              height: '150px',
+              backgroundColor: '#262D47',
+              borderRadius: '10px',
+              boxShadow: '0px 4px 8px rgba(0,0,0,0.1)',
+              padding: '10px',
+            }}
+        >
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', height: '100%' }}>
+            <div>
+            <img src="https://img.icons8.com/ios-filled/50/ffffff/sales-performance.png" alt="sales" style={{ width: '50px', height: '50px', marginLeft: '40%' }} />
+            </div>
+            <div>
+              <h3 style={{ textAlign: 'center', color: '#fff' }}>Profit</h3>
+              <p style={{ textAlign: 'center', color: '#fff' }}>{profit}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
+        <Box
+          style={{
+            width: '48%',
+            height: '430px',
+            backgroundColor: '#262D47',
+            borderRadius: '10px',
+            boxShadow: '0px 4px 8px rgba(0,0,0,0.1)',
+            padding: '10px',
+          }}
+        >
+          <Typography variant="h6" style={{ marginBottom: '10px', fontWeight: 'bold', color: '#fff' }}>
+            Sales by City
+          </Typography>
+          <Bar data={barChartData} options={barChartOptions} />
+        </Box>
+        <Box
+          style={{
+            width: '48%',
+            backgroundColor: '#262D47',
+            borderRadius: '10px',
+            boxShadow: '0px 4px 8px rgba(0,0,0,0.1)',
+            padding: '10px',
+            overflowY: 'auto',
+            height: '430px',
+          }}
+        >
+          <Typography variant="h6" style={{ marginBottom: '10px', fontWeight: 'bold', color: '#fff' }}>
+            Sales by Products
+          </Typography>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell style={{ fontWeight: 'bold' }}>Product Name</TableCell>
+                  <TableCell style={{ fontWeight: 'bold' }}>Sales in $</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {ProductNameResult.map((item) => (
+                  <TableRow key={item.product}>
+                    <TableCell>{item.product}</TableCell>
+                    <TableCell>{item.totalSales}</TableCell>
+                    </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </div>
+
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
+        <Box
+          style={{
+            width: '30%',
+            height: '430px',
+            backgroundColor: '#262D47',
+            borderRadius: '10px',
+            boxShadow: '0px 4px 8px rgba(0,0,0,0.1)',
+            padding: '10px',
+          }}
+        >
+          <Typography variant="h6" style={{ marginBottom: '10px', fontWeight: 'bold', color: '#fff' }}>
+            Sales by Category
+          </Typography>
+          <Pie data={pieChartData} />
+        </Box>
+
+        <Box
+          style={{
+            width: '30%',
+            backgroundColor: '#262D47',
+            borderRadius: '10px',
+            boxShadow: '0px 4px 8px rgba(0,0,0,0.1)',
+            padding: '10px',
+            height: '430px',
+            overflowY: 'auto',
+          }}
+        >
+          <Typography variant="h6" style={{ marginBottom: '10px', fontWeight: 'bold', color: '#fff' }}>
+            Sales by Sub-Category
+          </Typography>
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell style={{ fontWeight: 'bold' }}>Product Name</TableCell>
+                  <TableCell style={{ fontWeight: 'bold' }}>Sales in $</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {SubResult.map((item) => (
+                  <TableRow key={item.sub}>
+                    <TableCell>{item.sub}</TableCell>
+                    <TableCell>{item.totalSales}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Box>
+
+        <Box
+          style={{
+            width: '30%',
+            height: '430px',
+            backgroundColor: '#262D47',
+            borderRadius: '10px',
+            boxShadow: '0px 4px 8px rgba(0,0,0,0.1)',
+            padding: '10px',
+          }}
+        >
+          <Typography variant="h6" style={{ marginBottom: '10px', fontWeight: 'bold', color: '#fff' }}>
+            Sales by Segment
+          </Typography>
+          <Pie data={pieChartDataSegment} />
+        </Box>
+      </div>
+    </div>
+  );
+};
+
+export default MainContent;
